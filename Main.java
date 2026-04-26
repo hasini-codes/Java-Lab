8f618b24c994a55a19382e5dabdbb5c18b8589d9
Hasini Maheesha <hasinisepaladahanayaka@gmail.com>
2026-04-26 23:01:27 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,12 @@
-package Q13;
-import javax.swing.*;
-class SampleWindow {
-    public static void main(String[] args){
-        JFrame myWindow;
-        myWindow = new JFrame();
-        myWindow.setSize(500,250);
-        myWindow.setTitle("UOK");
-        myWindow.setVisible(true);
-        try {Thread.sleep(500);} catch(Exception e){ }
-        myWindow.setVisible(false);
-        try {Thread.sleep(500);} catch(Exception e) { }
-        myWindow.setVisible(true);
+package Q1;
+import javax.swing.JFrame;
 
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Welcome to Java");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-
 }
+
